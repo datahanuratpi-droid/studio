411c3beff992850e2a4015d2dac1cc0bae3cf80c
HanuraTPI <datahanuratpi@gmail.com>
2026-03-17 09:25:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7467824517-e863f",
+  "appId": "1:420330365207:web:9c4eb19b7ff954a9139752",
+  "apiKey": "AIzaSyCTLu_YdpFvbzeXvUHZYEaQsaVN-e7Xj9Y",
+  "authDomain": "studio-7467824517-e863f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "420330365207"
+};
